33ea985aac7f724c2e0b2fe39d4435ba66208f58
joyfun <joyfun@joyfun.net>
2020-05-27 22:52:01 +0800

    test corssplatform compile

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+#export C_COMPILER=aarch64-linux-gnu-gcc
+#export CXX_COMPILER=aarch64-linux-gnu-g++
 
 rm -rf _build
 mkdir _build
 cd _build
 cmake ..
+#cmake  -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ..
 make
